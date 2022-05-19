eb0cb576f4936c28b7b38cea173568db6d045864
xsuran07 <xsuran07@stud.fit.vutbr.cz>
2022-05-19 18:58:38 +0200

    Adopt access token fetching in App component
    The App component retrieves the token immediately after mount.
    Then it periodically refresh it using the setInterval function.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 import { SelectArtist, artistsList } from './components/SelectArtist';
 
+import { getToken } from "./shared/api";
+
 export default function App() {
   const [ artistID, setArtistID ] = useState(artistsList[0].id);
+  const [ token, setToken ] = useState('');
+  const [ fetchResult, setFetchResult ] = useState({ valid: false, loding: true });
+
+  useEffect(() =>{
+    const fetchToken = () => {
+      setFetchResult(prev => {
+         return { ...prev, loading: true }});
+      getToken(setToken, setFetchResult);
+    }
+
+    // fetch token when component mounts and periodically refresh it
+    fetchToken();
+    const interval = setInterval(fetchToken, 3500000)
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const mainConent = (fetchResult.valid) ?
+    <div>TODO</div> :
+    <Typography>Failed to get access token!</Typography>;
 
   return (
     <Stack
@@ -15,6 +38,9 @@ export default function App() {
       <h1>Spotify data visualization</h1>
       <SelectArtist artistID={artistID} setArtistID={setArtistID} />
       <Divider style={{ width: '100%' }}/>
+      {(fetchResult.loding &&
+        <Typography>Loading...</Typography>) || mainConent
+      }
     </Stack>
   );
 }
